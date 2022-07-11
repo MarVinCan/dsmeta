dd8435d7a3d45e3874e6237f14756de6730dbe1d
Marcus Vinicius Estevam Candido <marcus.v.e.candido@gmail.com>
2022-07-11 17:37:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NotificationButton from "./componets/NotificationButton"
 
 
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
