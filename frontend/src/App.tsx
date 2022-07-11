a1bb4a8a55b2421a1ae45e6060e082f25adf8b86
Marcus Vinicius Estevam Candido <marcus.v.e.candido@gmail.com>
2022-07-11 18:09:30 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
+import Header from "./componets/Header"
 import NotificationButton from "./componets/NotificationButton"
+import SalesCard from "./componets/SalesCard"
 
 
 function App() {
-  return(
+  return (
     <>
-    <h1>Olá mundo!</h1>
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard/>
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
